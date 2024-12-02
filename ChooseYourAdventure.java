a07d06bea8fd8f7393edd31eba2af8baa8827399
Kody <kody.vasquez@student.tamdistrict.org>
2024-12-01 20:58:59 -0800

    introduction

--- ChooseYourAdventure.java
+++ ChooseYourAdventure.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class ChooseYourAdventure {
     public static void main(String[] args) {
+        // Adding the scanner to the code
+        Scanner scanner = new Scanner(System.in);
+        // Introducing the project to the reader
+        System.out.println("Welcome to Choose Your Adventure! Today you will be making yourself a sandwich!");
 
     }
 
