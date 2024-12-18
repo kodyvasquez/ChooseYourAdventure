f3a0697d49b7c6109385971e1d6cde4c7a1008e5
Kody <kody.vasquez@student.tamdistrict.org>
2024-12-18 15:14:17 -0800

    Finished west mall

--- ChooseYourAdventure.java
+++ ChooseYourAdventure.java
@@ -23,18 +23,53 @@ public class ChooseYourAdventure {
                     System.out.println("You have been served your peporoni pizza");
                 }
                 if (pizzaChoice.equals("cheese") || pizzaChoice.equals("Cheese")) {
-                    System.out.println("You have been served your peporoni pizza");
+                    System.out.println("You have been served your cheese pizza");
 
                 }
                 if (restaurantChoice.equals("pasta") || restaurantChoice.equals("Pasta")) {
                     System.out.println("You have chosen Sally's Italian Diner");
-                    System.out.println("Do you want bolognese or butter pasta?");
+                    System.out.println("Do you want noodles or ravioli?");
                     String pastaChoice = scanner.nextLine();
+                    if (pastaChoice.equals("noodles") || pastaChoice.equals("Noodles")) {
+                        System.out.println("You have been served noodles.");
+                    }
+                    if (pastaChoice.equals("ravioli") || pastaChoice.equals("Ravioli")) {
+                        System.out.println("You have been served ravioli.");
+
+                    }
 
                 }
+
             }
 
         }
-
+        // This is the entire code if the user chooses West Mall
+        if (mallChoice.equals("West Mall") || mallChoice.equals("west mall")) {
+            System.out.println("You have chosen the West Mall");
+            System.out.println("Would you like to eat mexican or chinese?");
+            String secondRestaurantChoice = scanner.nextLine();
+            if (secondRestaurantChoice.equals("mexican") || secondRestaurantChoice.equals("Mexican")) {
+                System.out.println("You are now at Raphael's Tacos and Burritos");
+                System.out.println("Do you want quesedillas or tacos?");
+                String mexicanChoice = scanner.nextLine();
+                if (secondRestaurantChoice.equals("quesedillas") || secondRestaurantChoice.equals("Quesedillas")) {
+                    System.out.println("You have been served a quesedilla.");
+                }
+                if (secondRestaurantChoice.equals("tacos") || secondRestaurantChoice.equals("Tacos")) {
+                    System.out.println("You have been served tacos.");
+                }
+            }
+            if (secondRestaurantChoice.equals("chinese") || secondRestaurantChoice.equals("Chinese")) {
+                System.out.println("You are now at DJ's Chinese");
+                System.out.println("Do you want potstickers or lo mein?");
+                String chineseChoice = scanner.nextLine();
+                if (chineseChoice.equals("potstickers") || secondRestaurantChoice.equals("Potstickers")) {
+                    System.out.println("You have been served potstickers.");
+                }
+                if (chineseChoice.equals("lo mein") || secondRestaurantChoice.equals("Lo mein")) {
+                    System.out.println("You have been served lo mein.");
+                }
+            }
+        }
     }
 }
